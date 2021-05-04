a5fec238fad87e0da3752ee79020e1ae0c2de02f
ederSilvaDev <81658662+ederSilvaDev@users.noreply.github.com>
2021-05-03 22:11:42 -0300

    Projeto Felix De Cadastro de Alunos

---
+++ ProjetoFelixAulos-Web/src/main/java/br/com/eder/entidade/Aluno.java
@@ -0,0 +1,128 @@
+package br.com.eder.entidade;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Aluno implements Serializable {
+
+	private static final long serialVersionUID = -309513637403441918L;
+
+	private Long matricula;
+
+	private String nome;
+
+	private String telefone;
+
+	private String email;
+
+	private Date dataCadastro;
+
+	public Aluno() {
+
+	}
+
+	public Aluno(Long matricula) {
+		super();
+		this.matricula = matricula;
+	}
+
+	public Long getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(Long matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataCadastro == null) ? 0 : dataCadastro.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((matricula == null) ? 0 : matricula.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (dataCadastro == null) {
+			if (other.dataCadastro != null)
+				return false;
+		} else if (!dataCadastro.equals(other.dataCadastro))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (matricula == null) {
+			if (other.matricula != null)
+				return false;
+		} else if (!matricula.equals(other.matricula))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [matricula=" + matricula + ", nome=" + nome + ", telefone=" + telefone + ", email=" + email
+				+ ", dataCadastro=" + dataCadastro + "]";
+	}
+
+}
---
+++ ProjetoFelixAulos-Web/src/main/webapp/menu.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+<meta charset="ISO-8859-1">
+<title>Página Inicial</title>
+</head>
+<body>
+	<div align="center">
+		<H1>Seja bem vindo ao sistema AcademicNet!</H1>
+		<a href="aluno.jsp">Manter Aluno</a><br> <a
+			href="AlunoServlet?acao=Listar">Listar Alunos</a><br>
+	</div>
+
+</body>
+</html>
